7bf73c62e6d34dcf9ac3398b0ae86fc1e36d5b87
Kyle Barnett <kbarnett23@gmail.com>
2019-09-24 12:09:17 -0600

    connected components to initialState

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
+// import { connect } from 'http2';
+import { connect } from 'react-redux';
 
 const AdditionalFeatures = props => {
   return (
@@ -12,10 +14,16 @@ const AdditionalFeatures = props => {
           ))}
         </ol>
       ) : (
-        <p>Nice looking car!</p>
-      )}
+          <p>Nice looking car!</p>
+        )}
     </div>
   );
 };
 
-export default AdditionalFeatures;
+const mapStateToProps = state => {
+  return {
+    store: state.store
+  }
+}
+
+export default connect(mapStateToProps, {})(AdditionalFeatures);
