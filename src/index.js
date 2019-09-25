63a46b289876331f9f563b9824c1509330fdc292
Kyle Barnett <kbarnett23@gmail.com>
2019-09-24 22:26:01 -0600

    final MVP commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bulma/css/bulma.css';
 import './styles.scss';
 import { reducer } from './reducers';
 
-const store = createStore(reducer)
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
